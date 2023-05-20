bd24320ccce537fce431409d946a119f8d6fc145
ADRIAN RINCON <107867643+AdrianYRincon@users.noreply.github.com>
2023-05-20 08:57:16 -0500

    Update calendarApi.js

--- src/api/calendarApi.js
+++ src/api/calendarApi.js
@@ -4,7 +4,7 @@ import { getEnvVariables } from '../helpers/getEnvVariables';
 const { VITE_API_URL } = getEnvVariables();
 
 const calendarApi = axios.create({
-  baseURL: `${VITE_API_URL}/api`
+  baseURL: VITE_API_URL
 });
 
 // Todo: configurar interceptores
